76ad60476f146cfcea4a8aaa26b4e07b4026ae4d
Zach Sills <cosmicfactionss@gmail.com>
2018-07-25 17:08:10 -0700

    Tab thingy

--- src/main/java/net/practice/practice/listener/listeners/ServerListener.java
+++ src/main/java/net/practice/practice/listener/listeners/ServerListener.java
@@ -7,11 +7,16 @@ import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.wrappers.WrappedGameProfile;
+import net.minecraft.server.v1_8_R3.PacketPlayInTabComplete;
+import net.minecraft.server.v1_8_R3.PacketPlayOutTabComplete;
 import net.practice.practice.Practice;
 import net.practice.practice.util.chat.C;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class ServerListener {
@@ -22,7 +27,7 @@ public class ServerListener {
 
     private void loadPingListener() {
         final List<WrappedGameProfile> messages = new ArrayList<>();
-        messages.add(new WrappedGameProfile("1", C.color("&4&lNUB&C&LLAND&7[1.7 -> 1.8]")));
+        messages.add(new WrappedGameProfile("1", C.color("&4&lNUB&c&lLAND&7 [1.7 -> 1.8]")));
         messages.add(new WrappedGameProfile("2", C.color(" ")));
         messages.add(new WrappedGameProfile("3", C.color("&b&lTWITTER &f@NubLand")));
         messages.add(new WrappedGameProfile("4", C.color("&e&lFORUMS &fwww.nub.land")));
@@ -32,8 +37,32 @@ public class ServerListener {
 
         ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(Practice.getInstance(), ListenerPriority.NORMAL, Arrays.asList(PacketType.Status.Server.SERVER_INFO), ListenerOptions.ASYNC) {
             @Override
-            public void onPacketSending(final PacketEvent event) {
+            public void onPacketSending(PacketEvent event) {
                 event.getPacket().getServerPings().read(0).setPlayers(messages);
+            }
+        });
+
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(Practice.getInstance(), ListenerPriority.NORMAL, Arrays.asList(PacketType.Play.Server.TAB_COMPLETE), ListenerOptions.ASYNC) {
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                Player player = event.getPlayer();
+                if(player.isOp() || player.hasPermission("moderation.staff"))
+                    return;
+
+                List<String> stringList = new ArrayList<>(Arrays.asList(event.getPacket().getStringArrays().read(0)));
+                Iterator<String> it = stringList.iterator();
+                while(it.hasNext()) {
+                    String outgoing = it.next();
+                    Player other = Bukkit.getPlayer(outgoing);
+                    if(other == null || !other.isOnline())
+                        continue;
+
+                    land.nub.core.player.Profile coreProfile = land.nub.core.player.Profile.getByPlayer(other);
+                    if(coreProfile.getModMode() != null && coreProfile.getModMode().isVanished())
+                        it.remove();
+                }
+
+                event.getPacket().getStringArrays().write(0, stringList.toArray(new String[stringList.size()]));
             }
         });
     }
